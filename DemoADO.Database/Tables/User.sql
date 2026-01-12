b4b9819f67e93c9396d57aa44aff60e42927a890
Quentin Geerts <geerts.quentin@gmail.com>
2026-01-12 10:35:08 +0100

    Add project files.

---
+++ DemoADO.Database/Tables/User.sql
@@ -0,0 +1,41 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id] INT,
+	[Email] NVARCHAR(100) NOT NULL, 
+    [Password] NVARCHAR(255) NOT NULL,
+    [Lastname] NVARCHAR(50) NULL, 
+    [Firstname] NVARCHAR(50) NULL, 
+    [CreatedAt] DATETIME2 NULL DEFAULT GETDATE(), 
+    [UpdatedAt] DATETIME2 NULL DEFAULT GETDATE(), 
+    [IsActive] BIT NULL DEFAULT 1, 
+
+    CONSTRAINT PK_User PRIMARY KEY (Id), 
+    CONSTRAINT [CK_User_Email] CHECK (Email LIKE '%__@%__.%__')
+)
+
+GO
+
+CREATE TRIGGER [dbo].[TR_User_UpdatedAt]
+    ON [dbo].[User]
+    AFTER UPDATE
+    AS
+    BEGIN
+        SET NoCount ON
+        UPDATE [dbo].[User]
+        SET [UpdatedAt] = GETDATE()
+        WHERE [Id] = (SELECT [Id] FROM inserted)
+    END
+GO
+
+-- Déclencheur qui remplace le comportement par défaut d'une suppression
+-- Par la désactivation de celui-ci (Soft-delete)
+CREATE TRIGGER [dbo].[TR_User_IsActive]
+    ON [dbo].[User]
+    INSTEAD OF DELETE
+    AS
+    BEGIN
+        SET NoCount ON
+        UPDATE [dbo].[User]
+        SET [IsActive] = 0
+        WHERE [Id] = (SELECT [Id] FROM deleted)
+    END
---
+++ DemoADO/Program.cs
@@ -0,0 +1,8 @@
+﻿/*
+ *  Démonstration ADO
+ */
+
+
+// 1.  Création d'un projet: Projet de base de données SQL Server
+// 1.1.  Création des tables
+
