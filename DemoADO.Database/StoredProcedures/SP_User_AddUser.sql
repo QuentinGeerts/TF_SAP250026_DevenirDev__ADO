0dbf5cfb15399636c0bf9aa2a2933137b6878f1c
Quentin Geerts <geerts.quentin@gmail.com>
2026-01-12 12:18:43 +0100

    Création des procédures stockées

---
+++ DemoADO.Database/StoredProcedures/SP_User_AddUser.sql
@@ -0,0 +1,31 @@
+﻿CREATE PROCEDURE [dbo].[SP_User_AddUser]
+	@Email NVARCHAR(100),
+	@Password NVARCHAR(255),
+	@Lastname NVARCHAR(50) = NULL,
+	@Firstname NVARCHAR(50) = NULL,
+	@UserId INT
+AS
+BEGIN
+
+	SET NOCOUNT ON
+
+	BEGIN TRY
+
+		IF EXISTS (SELECT 1 FROM [dbo].[User] WHERE [Email] = @Email)
+		BEGIN
+			RAISERROR('Email already exists.', 16, 13)
+		END
+
+		INSERT INTO [dbo].[User] (Email, Password, Lastname, Firstname)
+		OUTPUT inserted.Id INTO @UserId
+		VALUES (@Email, HASHBYTES('SHA2_256', @Password), @Lastname, @Firstname)
+
+	END TRY
+
+	BEGIN CATCH
+		THROW;
+	END CATCH
+
+	SET NOCOUNT OFF
+
+END
--- DemoADO/Program.cs
+++ DemoADO/Program.cs
@@ -5,4 +5,5 @@
 
 // 1.  Création d'un projet: Projet de base de données SQL Server
 // 1.1.  Création des tables
-
+// 1.2.  Création des vues
+// 1.3.  Création des procédures stockées
