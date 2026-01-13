0a5fcf8d83d5bad86d0f22d24330576dc1745a22
Quentin Geerts <geerts.quentin@gmail.com>
2026-01-13 09:58:54 +0100

    Correction exercice page 61

---
+++ ExercicePage61/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+ *  Exercice Page 61
+ *  Établissez la connexion à votre base de données « ADO »
+ */
+
+using Microsoft.Data.SqlClient;
+
+string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ExerciceADO;Integrated Security=True;Trust Server Certificate=True";
+
+using (SqlConnection connection = new SqlConnection(connectionString))
+{
+    connection.Open();
+    Console.WriteLine($"Connexion établie");
+}
