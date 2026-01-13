b6db69ce36a87ee2a206c31a9e8b3004dda99a77
Quentin Geerts <geerts.quentin@gmail.com>
2026-01-13 16:06:26 +0100

    Correction exercice page 86

---
+++ ExercicePage86/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿namespace ExercicePage86.Models;
+
+internal class Student
+{
+    public Student(string lastname, string firstname, DateTime birthDate, int? yearResult, int sectionId)
+    {
+        Lastname = lastname;
+        Firstname = firstname;
+        BirthDate = birthDate;
+        YearResult = yearResult;
+        SectionId = sectionId;
+    }
+
+    public string Lastname { get; set; }
+    public string Firstname { get; set; }
+    public DateTime BirthDate { get; set; }
+    public int? YearResult { get; set; }
+    public int SectionId { get; set; }
+}
---
+++ ExercicePage86/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Exercice Page 86
+ * ◼ Instanciez un objet de type « Student » contenant vos informations
+ * ◼ Insérez votre objet en base de données en récupérant son « ID » au passage
+ */
+
+using ExercicePage86.Models;
+using Microsoft.Data.SqlClient;
+
+Student quentin = new Student("Geerts", "Quentin", new DateTime(1996, 4, 3), 12, 1010);
+
+
+string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ExerciceADO;Integrated Security=True;Trust Server Certificate=True";
+
+using (SqlConnection connection = new SqlConnection(connectionString))
+{
+    using (SqlCommand cmd = connection.CreateCommand())
+    {
+        cmd.CommandText = $"INSERT INTO Student (FirstName, LastName, BirthDate, YearResult, SectionId) " +
+            $"OUTPUT inserted.Id " +
+            $"VALUES ('{quentin.Firstname}', '{quentin.Lastname}', '{quentin.BirthDate.ToString("yyyy-MM-dd")}', " +
+            $"{(object)quentin.YearResult ?? "NULL"}, {quentin.SectionId})";
+
+        connection.Open();
+        int id = (int)cmd.ExecuteScalar();
+        connection.Close();
+
+        Console.WriteLine($"Étudiant inséré avec succès, id={id}");
+    }
+}
+
